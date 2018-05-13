6aa3da3846c7185f115a83b5d89f3d22e6a95662
piotrek <pszklanko@gmail.com>
2018-05-13 21:17:17 +0200

    select hero and show details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,10 +9,7 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
   heroes = HEROES;
 
   constructor() {
@@ -21,4 +18,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
